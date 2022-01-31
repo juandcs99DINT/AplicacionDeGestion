7ba7220f226cf07b18e89136a982ee747c6be863
Juandi <Juandi@DESKTOP-LVHFGRV>
2022-01-31 18:55:06 +0100

    añado ObservableObject a todas las VM, quizá haya que cambiar alguna en un futuro por ObservableRecipient

--- AplicacionDeGestion/viewmodels/MainWindowVM.cs
+++ AplicacionDeGestion/viewmodels/MainWindowVM.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using AplicacionDeGestion.servicios;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace AplicacionDeGestion.viewmodels
 {
-    class MainWindowVM
+    class MainWindowVM : ObservableObject
     {
+        private readonly NavigationService navigationService;
+        public MainWindowVM()
+        {
+            navigationService = new NavigationService();
+            CambiarAClientesCommand = new RelayCommand(CambiarAClientes);
+            CambiarAVehiculosCommand = new RelayCommand(CambiarAVehiculos);
+            CambiarAEstacionamientosCommand = new RelayCommand(CambiarAEstacionamientos);
+        }
+
+        private UserControl userControlActual;
+        public UserControl UserControlActual
+        {
+            get => userControlActual;
+            set => SetProperty(ref userControlActual, value);
+        }
+
+        public RelayCommand CambiarAClientesCommand { get; }
+        public RelayCommand CambiarAVehiculosCommand { get; }
+        public RelayCommand CambiarAEstacionamientosCommand { get; }
+
+        public void CambiarAClientes() => UserControlActual = navigationService.CambiarAPestañaClientes();
+        public void CambiarAVehiculos() => UserControlActual = navigationService.CambiarAPestañaVehiculos();
+        public void CambiarAEstacionamientos() => UserControlActual = navigationService.CambiarAPestañaEstacionamientos();
+
+
+
+
     }
 }
