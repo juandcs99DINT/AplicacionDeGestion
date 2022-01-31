99b22a1446309a5ff61639b0745b79cc932e6cc5
Juan Diego <jd.cabrera@iesdoctorbalmis.com>
2022-01-31 14:51:40 +0100

    Creación archivos VM, Vistas, Servicios, añado paquetes NuGet de API y Json. Edito XAML CrearModificarUsuario.xaml para que sea un diálogo.

---
+++ AplicacionDeGestion/servicios/AzureServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionDeGestion.servicios
+{
+    class AzureServices
+    {
+        // Aquí va lo del Servicio Face y lo del Blob Storage
+        // Lo del Custom Vision y Computer Vision va en la otra aplicación
+    }
+}
---
+++ AplicacionDeGestion/servicios/DialogosService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionDeGestion.servicios
+{
+    class DialogosService
+    {
+        // Aquí se harían los MessageBox y los OpenFileDialog para las imágenes
+    }
+}
